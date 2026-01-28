1015fa9d1d03398ce930c61ceac8938af80e01b5
weucode <2452211988@qq.com>
2026-01-28 07:21:11 +0000

    20260125 15:21

--- workspace/anomaly_processor.py
+++ workspace/anomaly_processor.py
@@ -4,14 +4,15 @@ import re
 
 
 logging.basicConfig(level=logging.INFO, filemode="w", filename="anomaly_processor.log")
-false_positive, real_bug, unprocessed = [], {}, []
+false_positive, real_bug, unprocessed = {}, {}, []
+performance_anomalies = 0
 
 
-def update_real_bugdict(bug_type, output_id):
-    if bug_type in real_bug:
-        real_bug[bug_type].append(output_id)
+def update_dict(d, k, v):
+    if k in d:
+        d[k].append(v)
     else:
-        real_bug[bug_type] = [output_id]
+        d[k] = [v]
 
 
 def classify(testcase, outputs, output_id, testbed_id, bug_type):
@@ -24,161 +25,186 @@ def classify(testcase, outputs, output_id, testbed_id, bug_type):
                     "var eval;", "console."]
     for es6_feature in es6_features:
         if es6_feature in testcase:
-            false_positive.append(output_id)
+            # false_positive.append(output_id)
+            update_dict(false_positive, "ES6", output_id)
             return True
             break
     if "Math." in testcase and testbed_id in [6, 12]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "ES6", output_id)
         return True
     if re.search(r"(var|const) *\{.*?\} *=", testcase) is not None:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "ES6", output_id)
         return True
     if re.search(r",\n *\)", testcase) is not None:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "ES6", output_id)
         return True
     if re.search(r"for.*? of ", testcase) is not None:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "ES6", output_id)
         return True
     if re.search(r"var.*\[.*\].*\[.*\]", testcase) is not None and testbed_id in [5, 6, 11, 12]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "ES6", output_id)
         return True
     if len(outputs) == 6 and testbed_id in [6, 12] and "unexpected token" in outputs[5][5]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "ES6", output_id)
         return True
     if re.search(r"{[\s\S]+\[[\s\S]+\]:[\s\S]+}", testcase) is not None and testbed_id in [6, 12]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "ES6", output_id)
         return True
     if re.search(r"\[[\s\S]+\[.*?\][\s\S]+\]", testcase) is not None and testbed_id in [5, 6, 11, 12]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "ES6", output_id)
         return True
     # Check if this is a false positive triggered by common reasons
-    false_positive_features = [ "Math.random", "Date", "setTimeout", "clearTimeout", "setInterval",
-                                "catch {", "global.", ", eval)(", "this.assert", ".name"]
-    for false_positive_feature in false_positive_features:
+    false_positive_functional_features = [ "Date", "catch {", "global.", ", eval)(", "this.assert", ".name"]
+    false_positive_performance_features = ["setTimeout", "clearTimeout", "setInterval"]
+    for false_positive_feature in false_positive_functional_features:
         if false_positive_feature in testcase:
-            false_positive.append(output_id)
+            # false_positive.append(output_id)
+            update_dict(false_positive, "functional", output_id)
+            return True
+    for false_positive_feature in false_positive_performance_features:
+        if false_positive_feature in testcase:
+            # false_positive.append(output_id)
+            update_dict(false_positive, "tmp", output_id)
             return True
     if "Error.prepareStackTrace" in testcase and testbed_id in [1, 7]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     if len(outputs) == 6 and testbed_id in [6, 12] and re.search(r"[-\d\.]+", outputs[5][4]) is not None and bug_type == "Pass value *** run error":
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     if testbed_id in [3, 9] and re.search(r"[-\d\.]+", outputs[2][4]) is not None and bug_type == "Pass value *** run error":
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     if re.search(r"function *\(.*=.*\)", testcase) is not None:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     if "arguments[0]" in testcase and testbed_id in [6, 12]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     if ".substr(" in testcase and testbed_id in [6, 12]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     if bug_type == "Excessive time difference" and "string method mutation" in testcase:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "performance", output_id)
         return True
     if ".values" in testcase and testbed_id in [5, 6, 11, 12] and bug_type == "Pass value *** run error":
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     if "debugger" in testcase and testbed_id in [6, 12]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     if "this.write" in testcase and testbed_id in [6, 12]:
-        false_positive.append(output_id)
+        # false_positive.append(output_id)
+        update_dict(false_positive, "functional", output_id)
         return True
     # Check if this is a real bug
     if "//regex method mutation" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs admit an invalid regular expression", output_id)
+        update_dict(real_bug, "mujs admit an invalid regular expression", output_id)
         return True
     if len(outputs) == 6 and "regexec failed" in outputs[5][5] and (testbed_id in [6, 12]):
-        update_real_bugdict("mujs throw exception with a regular expression1", output_id)
+        update_dict(real_bug, "mujs throw exception with a regular expression1", output_id)
         return True
     if len(outputs) == 6 and "regular expression" in outputs[5][5] and (testbed_id in [6, 12]):
-        update_real_bugdict("mujs throw exception with a regular expression2", output_id)
+        update_dict(real_bug, "mujs throw exception with a regular expression2", output_id)
         return True
     if testbed_id in [3, 9] and outputs[2][3] == 10:
-        update_real_bugdict("jerryscript returncode 10", output_id)
+        update_dict(real_bug, "jerryscript returncode 10", output_id)
         return True
     if "JSON.stringify" in testcase and testbed_id in [3, 9]:
-        update_real_bugdict("jerryscript returncode 10", output_id)
+        update_dict(real_bug, "jerryscript returncode 10", output_id)
         return True
     if "use strict" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs behave differently when strict mode is enabled", output_id)
+        update_dict(real_bug, "mujs behave differently when strict mode is enabled", output_id)
         return True
     if "++ /" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs incorrectly parse '++ /'", output_id)
+        update_dict(real_bug, "mujs incorrectly parse '++ /'", output_id)
         return True
     if "Object.keys" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs does not box the original value", output_id)
+        update_dict(real_bug, "mujs does not box the original value", output_id)
         return True
     if "this.hasOwnProperty" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs does not support 'this.hasOwnProperty'", output_id)
+        update_dict(real_bug, "mujs does not support 'this.hasOwnProperty'", output_id)
         return True
     if "Object.defineProperty" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs behave differently when using Object.defineProperty", output_id)
+        update_dict(real_bug, "mujs behave differently when using Object.defineProperty", output_id)
         return True
     if "__proto__: obj.__proto__" in testcase and "Object.getOwnPropertyNames" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs throw exception", output_id)
+        update_dict(real_bug, "mujs throw exception", output_id)
         return True
     if "__proto__: obj.__proto__" in testcase and "Object.getOwnPropertyNames" in testcase and "[object Object]" in outputs[4][4]:
-        update_real_bugdict("duktape output different result", output_id)
+        update_dict(real_bug, "duktape output different result", output_id)
         return True
     if "Object.assign" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs does not transform true into a Boolean Object", output_id)
+        update_dict(real_bug, "mujs does not transform true into a Boolean Object", output_id)
         return True
     if "config.anchor.x" in testcase and testbed_id in [2, 8]:
-        update_real_bugdict("quickjs run successfully while other engines throw exception", output_id)
+        update_dict(real_bug, "quickjs run successfully while other engines throw exception", output_id)
         return True
     if "Object.create(null)" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs does not throw exception when create a new object with null prototype", output_id)
+        update_dict(real_bug, "mujs does not throw exception when create a new object with null prototype", output_id)
         return True
     if outputs[3][3] == 1 and "undefined variable" in outputs[3][5] and "for (var INDEX = 0; INDEX < 1000; INDEX++)" in testcase:
-        update_real_bugdict("xs optimize a function definition incorrectly", output_id)
+        update_dict(real_bug, "xs optimize a function definition incorrectly", output_id)
         return True
     if "this.global" in testcase and testbed_id in [4, 10]:
-        update_real_bugdict("xs defines this.global", output_id)
+        update_dict(real_bug, "xs defines this.global", output_id)
         return True
     if "Object.defineProperties" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs does not throw exception when transforming a wrong value to Object.defineProperties", output_id)
+        update_dict(real_bug, "mujs does not throw exception when transforming a wrong value to Object.defineProperties", output_id)
         return True
     if "esModule" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs throw exception when xxx", output_id)
+        update_dict(real_bug, "mujs throw exception when xxx", output_id)
         return True
     if "arguments[0]" in testcase and testbed_id in [1, 7]:
-        update_real_bugdict("hermes does not support arguments", output_id)
+        update_dict(real_bug, "hermes does not support arguments", output_id)
         return True
     if ".apply(" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs does not check variable type when calling apply", output_id)
+        update_dict(real_bug, "mujs does not check variable type when calling apply", output_id)
         return True
     if "Object.getOwnPropertyDescriptor" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs behave differently when calling getOwnPropertyDescriptor", output_id)
+        update_dict(real_bug, "mujs behave differently when calling getOwnPropertyDescriptor", output_id)
         return True
     if ".link" in testcase and testbed_id in [2, 8]:
-        update_real_bugdict("jerryscript behave differently when calling link", output_id)
+        update_dict(real_bug, "jerryscript behave differently when calling link", output_id)
         return True
     if "JSON.stringify" in testcase and "localeCompare" in testcase and testbed_id in [4, 6, 10, 12]:
-        update_real_bugdict("xs and mujs output incorrect results", output_id)
+        update_dict(real_bug, "xs and mujs output incorrect results", output_id)
         return True
     if "eval" in testcase and testbed_id in [1, 7]:
-        update_real_bugdict("hermes behave diferently when calling eval", output_id)
+        update_dict(real_bug, "hermes behave diferently when calling eval", output_id)
         return True
     if "arr.indexOf(true, Infinity)" in testcase and testbed_id in [5, 11]:
-        update_real_bugdict("duktape incorrectly handle a extremely long array", output_id)
+        update_dict(real_bug, "duktape incorrectly handle a extremely long array", output_id)
         return True
     if "a === ++a" in testcase and testbed_id in [5, 11]:
-        update_real_bugdict("duktape use wrong calculation order", output_id)
+        update_dict(real_bug, "duktape use wrong calculation order", output_id)
         return True
     if " = Object.create(" in testcase and testbed_id in [6, 12]:
-        update_real_bugdict("mujs outputs different results when calling Object.create", output_id)
+        update_dict(real_bug, "mujs outputs different results when calling Object.create", output_id)
         return True
     # If not satisfied above conditions
     return False
 
 
 def main():
-    global false_positive, real_bug, unprocessed
+    global false_positive, real_bug, unprocessed, performance_anomalies
 
     db_file = 'fuzzli-ablation.db'
     # db_file = 'fuzzlil.db'
@@ -198,6 +224,8 @@ def main():
         sql4 = f"SELECT testcase FROM Testcases where id = {testcase_id};"
         cursor.execute(sql4)
         testcase = cursor.fetchone()[0]
+        if bug_type in ["Timeout", "Excessive time difference"]:
+            performance_anomalies += 1
         processed = classify(testcase, outputs, output_id, testbed_id, bug_type)
         if not processed:
             unprocessed.append(output_id)
@@ -210,13 +238,18 @@ def main():
     # processed = []
     # todo = []
     unprocessed = list(set(unprocessed) - set(processed) - set(todo))
-    print("false_positive:", len(false_positive))
+    print("false_positive_functional:", len(false_positive["functional"]))
+    print("false_positive_performance:", len(false_positive["performance"]))
+    print("false_positive_es6:", len(false_positive["ES6"]))
+    false_positive_performance_rate = len(false_positive["performance"]) / len(output_ids)
+    print("false_positive_performance_rate:", false_positive_performance_rate)
     print("real_bug:", len(real_bug))
     print("unprocessed:", len(unprocessed))
     # 按升序排序
     unprocessed = sorted(unprocessed)
     logging.info("unprocessed:"+str(unprocessed))
     logging.info("real_bug:"+str(real_bug))
+    print("performance_anomalies:", performance_anomalies)
 
 if __name__ == "__main__":
     main()
